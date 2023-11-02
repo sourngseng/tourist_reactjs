42597a061664cda4a6cd88431869acabce7cb882
Seng Sourng <sengsourng@istc.edu.kh>
2023-11-02 12:16:48 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Menu } from "./components/Menu";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Suspense } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = () => {
+  return (
+    <Suspense fallback="loading">
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  );
+};
+export default App;
---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const LANGUAGES = [
+    { label: 'ខ្មែរ', code: 'kh' },
+    { label: 'Spanish', code: 'es' },
+    { label: 'English', code: 'en' },
+    { label: 'Italian', code: 'it' },
+]
